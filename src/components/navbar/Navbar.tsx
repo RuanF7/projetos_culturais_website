26f378aea4c3391ad95da794da462ce5dba6c813
Ruan Finamor <ruanfinamor@gmail.com>
2023-04-22 17:15:14 -0300

    feat: navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,16 +1,46 @@
-import { Box } from "@mui/material"
-
+import { AppBar, Tab, IconButton, InputBase, Paper, Tabs, Toolbar, Typography } from "@mui/material"
+import SearchIcon from '@mui/icons-material/Search';
+import { useState } from "react";
 
 function Navbar() {
+  const [value, setValue] = useState();
+
   return (
-    <nav className="navbar"> 
-      <Box>
-        <Box>Home</Box>
-        <Box>Gravataí: entre anjos e Gravatás </Box>
-        <Box>CECI: a menina que podia voar</Box>
-        <Box>SPMG: uma história de luta</Box>
-      </Box>
-    </nav>
+    <>
+      <AppBar
+        sx={{
+          backgroundColor: '#FEF279',
+        }}
+      >
+        <Toolbar>
+          <Tabs textColor="inherit"
+            value={value}
+            onChange={(e, value) => setValue(value)}
+            indicatorColor="secondary"
+          >
+            <Tab sx={{ color: '#000000' }} label='Home' />
+            <Tab sx={{ color: '#000000' }} label='Gravataí: entre anjos e Gravatás' />
+            <Tab sx={{ color: '#000000' }} label='CECI: a menina que podia voar' />
+            <Tab sx={{ color: '#000000' }}
+              label='SPMG: uma história de luta' />
+          </Tabs>
+          <Paper
+            component="form"
+            sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 200 }}
+          >
+            <InputBase
+              sx={{ ml: 1, flex: 1 }}
+              placeholder="Pesquisar"
+              inputProps={{ 'aria-label': 'search' }}
+            />
+            <IconButton type="button" sx={{ p: '10px' }} aria-label="search">
+              <SearchIcon />
+            </IconButton>
+          </Paper>
+        </Toolbar>
+
+      </AppBar>
+    </>
   )
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,11 @@
 
-import { Inter } from 'next/font/google'
+import Navbar from '@/components/navbar/Navbar'
 
 export default function Home() {
   return (
     <>
       <main>
+        <Navbar />
         <h1>
           Hello World!
         </h1>
