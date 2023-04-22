77df8b64b19057f921ae1531013f932101fc03e1
Ruan Finamor <ruanfinamor@gmail.com>
2023-04-22 15:38:07 -0300

    first commit

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,17 @@
+import { Box } from "@mui/material"
+
+
+function Navbar() {
+  return (
+    <nav className="navbar"> 
+      <Box>
+        <Box>Home</Box>
+        <Box>Gravataí: entre anjos e Gravatás </Box>
+        <Box>CECI: a menina que podia voar</Box>
+        <Box>SPMG: uma história de luta</Box>
+      </Box>
+    </nav>
+  )
+}
+
+export default Navbar
