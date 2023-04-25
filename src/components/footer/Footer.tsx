2adc56fec358c33b6368da1558eb0d68283c4ab2
Ruan Finamor <ruanfinamor@gmail.com>
2023-04-25 17:49:28 -0300

    feat: social links

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -3,6 +3,7 @@ import FacebookIcon from '@mui/icons-material/Facebook';
 import EmailIcon from '@mui/icons-material/Email';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import ExpandCircleDownIcon from '@mui/icons-material/ExpandCircleDown';
+import Link from "next/link";
 
 
 function Footer() {
@@ -26,18 +27,43 @@ function Footer() {
           justifyContent: 'center',
           flexDirection: 'column',
           textAlign: 'center',
-          
+
 
         }}
       >
         <Typography sx={{ fontSize: 30, fontWeight: 'bold' }} >
-          Siga-me          
+          Siga-me
         </Typography>
         <ExpandCircleDownIcon sx={{ fontSize: 25 }} />
         <Box sx={{ padding: 2, marginTop: 3 }}>
-          <EmailIcon sx={{ fontSize: 41 }}  />
-          <FacebookIcon sx={{ fontSize: 40, marginLeft: 1 }} />
-          <InstagramIcon sx={{ fontSize: 40, marginLeft: 1 }} />
+          <a href='mailto:irmastm.pc@gmail.com'
+          target="_blank"
+          style={{
+            textDecoration: 'none',
+            color: 'black',
+          }}          
+          >
+            <EmailIcon sx={{ fontSize: 41 }} />
+          </a>
+          <a href='https://www.facebook.com/projetosculturaisirmastm'
+          target="_blank"
+          style={{
+            textDecoration: 'none',
+            color: 'black',
+          }} 
+          >
+            <FacebookIcon sx={{ fontSize: 40, marginLeft: 1 }} />
+          </a>
+          <a href='https://www.instagram.com/projetosculturaisirmastm'
+          target="_blank"
+          style={{
+            textDecoration: 'none',
+            color: 'black',
+          }} 
+          >
+            <InstagramIcon sx={{ fontSize: 40, marginLeft: 1 }} />
+          </a>
+
         </Box>
         <Typography sx={{ fontWeight: 'bold' }}>
           © 2021 por Irmãs TM - Projetos Culturais
