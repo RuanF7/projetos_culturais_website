06747cfbf9ef3b43bdf0ef028f0f10c4c485891c
Ruan Finamor <ruanfinamor@gmail.com>
2023-05-01 10:41:27 -0300

    feat: imagens e pagina gravataí

---
+++ src/components/gravataiButtons/GravataiButtons.tsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import CardLogo from '@/components/cardLogo/CardLogo'
+import { Box, Button, Grid } from '@mui/material'
+
+function GravataiButtons() {
+  return (
+    <>
+      <CardLogo /><Grid container spacing={2}>
+        <Grid xs={2}>
+          <Box sx={{ position: "fixed", paddingTop: 40, display: 'flex', flexDirection: 'column', marginLeft: 2, }}>
+            <Button
+              sx={{
+                fontSize: 12,
+                backgroundColor: 'primary.main',
+                color: 'warning.main',
+                width: 250,
+                height: 50,
+                margin: 1,
+                borderRadius: 3,
+              }}
+            >GRAVATAÍ: entre anjos e gravatás</Button>
+            <Button
+              sx={{
+                fontSize: 12,
+                backgroundColor: 'primary.main',
+                color: 'warning.main',
+                width: 250,
+                height: 50,
+                margin: 1,
+                borderRadius: 3,
+              }}
+            >Vídeos e Entrevistas</Button>
+            <Button
+              sx={{
+                fontSize: 12,
+                backgroundColor: 'primary.main',
+                color: 'warning.main',
+                width: 250,
+                height: 50,
+                margin: 1,
+                borderRadius: 3,
+              }}
+            >Lugares de Gravataí</Button>
+          </Box>
+        </Grid>
+      </Grid>
+    </>
+  )
+}
+
+export default GravataiButtons
--- src/pages/gravatai/Gravatai.tsx
+++ src/pages/gravatai/Gravatai.tsx
@@ -1,8 +1,92 @@
+
+import GravataiButtons from '@/components/gravataiButtons/GravataiButtons'
+import { Box, Button, Typography } from '@mui/material'
 import React from 'react'
+import Image from 'next/image'
+import YouTubeIcon from '@mui/icons-material/YouTube';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import FacebookIcon from '@mui/icons-material/Facebook';
 
 function Gravatai() {
   return (
-    <div>Gravatai</div>
+    <>
+      <GravataiButtons />
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          flexDirection: 'column',
+          marginLeft: 10,
+        }}
+      >
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+            padding: 10,
+          }}
+        >
+          <Box>
+            <Image
+              src="/gravatai.png"
+              alt="Imagem do livro Gravataí"
+              width={391}
+              height={200}
+            />
+            <Box sx={{ width: 500, paddingTop: 5, textAlign: 'center', }}>
+              <Typography>
+                O projeto "Gravataí: entre anjos e gravatás" realizou diversas entrevistas como atividade complementar ao livro, elas podem ser conferidas no nosso canal do Youtube e nas nossas redes sociais Instagram e Facebook.
+              </Typography>
+              <Box sx={{paddingTop: 5,}}>
+                <Button
+                  sx={{
+                    fontSize: 15,
+                    backgroundColor: 'secondary.main',
+                    color: 'warning.light',
+                    width: 150,
+                    margin: 1,
+                    borderRadius: 3,
+                  }}
+                  endIcon={< InstagramIcon sx={{ size: 'large' }} />}
+                >
+                  Instagram
+                </Button>
+                <Button
+                  sx={{
+                    fontSize: 15,
+                    backgroundColor: 'secondary.main',
+                    color: 'warning.light',
+                    width: 150,
+                    margin: 1,
+                    borderRadius: 3,
+                  }}
+                  endIcon={< FacebookIcon />}
+                >
+                  Facebook
+                </Button>
+                <Button
+                  sx={{
+                    fontSize: 15,
+                    backgroundColor: 'secondary.main',
+                    color: 'warning.light',
+                    width: 150,
+                    margin: 1,
+                    borderRadius: 3,
+                  }}
+                  endIcon={< YouTubeIcon />}
+                >
+                  Youtube
+                </Button>
+              </Box>
+            </Box>
+          </Box>
+        </Box>
+      </Box>
+    </>
+
+
   )
 }
 
