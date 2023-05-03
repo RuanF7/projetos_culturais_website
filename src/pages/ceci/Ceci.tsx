5e1f1b71496507a8f431a6a71621fa80f97bce30
Ruan Finamor <ruanfinamor@gmail.com>
2023-05-03 09:48:53 -0300

    refactor: ceci image

--- src/pages/ceci/Ceci.tsx
+++ src/pages/ceci/Ceci.tsx
@@ -49,7 +49,7 @@ function Ceci() {
                   className={styles.imagem}
                   src="/imagensCeci/ceci.webp"
                   alt="Imagem do livro Gravataí"
-                  width={150}
+                  width={180}
                   height={200} />
                 O projeto consiste na fabricação de um livro infantil, focado em crianças em fase de alfabetização.
                 A obra narra a história de Cecília – cujo apelidado é Ceci – uma menina de cinco anos, muito curiosa, que não consegue compreender a chegada de seu irmão mais novo, Joaquim, na família. O conto aborda o elemento  metafórico de que o pensamento nos faz voar. Portanto, com diversos questionamentos em sua mente, devido as novidades trazidas pela presença de mais uma pessoa em sua vida, Ceci consegue voar.Durante sua viagem sobre o planeta Terra ela observa diversos animais e suas relações enquanto grupo, o que faz com que a pequena perceba a importância do novo papel de sua vida: ser uma irmã mais velha.
