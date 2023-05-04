28dbe96e6f6f4884a9a95b86ed7953e44c7e55c8
Ruan Finamor <ruanfinamor@gmail.com>
2023-05-04 17:24:08 -0300

    feat: video apresentação do projeto

--- src/pages/spmg/Spmg.tsx
+++ src/pages/spmg/Spmg.tsx
@@ -1,6 +1,8 @@
 import SpmgButtons from '@/components/spmgButtons/SpmgButtons'
 import { Box, Typography } from '@mui/material'
 import React from 'react'
+import Image from 'next/image'
+
 
 function Spmg() {
   return (
@@ -63,9 +65,52 @@ function Spmg() {
             >Uma história de luta
             </Typography>
           </Box>
+          <Box
+            sx={{
+              display: 'flex',
+              justifyContent: 'center',
+              flexDirection: 'column',
+              color: 'warning.main',
+              width: 500,
+              margin: 15,
+            }}>
+            <Typography
+              sx={{
+                fontSize: 20,
+                textAlign: 'left',
+                textIndent: 20,
+              }}>
+              Exposição virtual que apresenta a trajetória do Sindicato dos Trabalhadores em Educação Pública Municipal de Gravataí – SPMG.
+              Projeto contemplado e financiado com recursos da Lei Federal 14.017/2020 - Lei Aldir Blanc em Gravataí/RS. Edital Prêmio Ocupação de Território 011/2020.
+            </Typography>
+
+            <Typography
+              sx={{
+                fontSize: 20,
+                textAlign: 'left',
+                textIndent: 20,
+              }}>
+              Aqui encontram-se objetos das campanhas realizadas pelo sindicato: camisetas, cartazes, adesivos, fotografias das manifestações, exemplares originais dos jornais “O Caleidoscópio”, entre outros itens desenvolvidos e/ou preservados pela instituição desde 1988.
+
+            </Typography>
+            <Typography
+              sx={{
+                fontSize: 20,
+                textAlign: 'left',
+                textIndent: 20,
+              }}>
+              Lembramos que nenhum direito é adquirido, eles são conquistas. Foram conquistados por meio de lutas, do empenho e da dedicação de diversos profissionais que fazem parte dessa e de tantas outras histórias, por isso SPMG é mais do que nunca: uma história de luta.
+            </Typography>
+          </Box>
+          <a href='https://www.youtube.com/watch?v=atgnSDcJ46c'
+            target="_blank">
+            <Image src='/imagensSPMG/helenaVideo.webp'
+              alt="Imagem do Video Apresentação do projeto"
+              width={700}
+              height={400} />
+          </a>
         </Box>
       </Box>
-
     </>
   )
 }
